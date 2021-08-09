d8ab45c8cd66a74d0c8bb9d29a39671480838147
DESKTOP-3OGR79T\seulAh <seulAh0729@github.com>
2021-08-09 23:59:11 +0900

    Reusable Components with JSX + Props <0809>

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import React from 'react';
 - component는 HTML을 반환하는 함수.
 
 */
+function Food (){
+
+  return <h1> I'dont Like Potato</h1>
+
+}
+
+/**
+ *  
+ * jsx 
+ * 
+ */
 
 
 function App() {
   return (
   <div>
     <h1>Hello!!!!!!!</h1>
+    <Food name = "Kimchi" />   
   </div>
-  );
+  ); // food component에 kimchi 라는 value로 prop name을 줌
 }
 
 export default App;
